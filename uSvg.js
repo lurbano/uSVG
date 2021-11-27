46699d386252f7d05b7bba5e86f81dc611e77db6
lurbano <lurbano@earthsciweb.org>
2021-11-27 17:57:03 -0600

    labelLinearFunction

--- uSvg.js
+++ uSvg.js
@@ -192,7 +192,7 @@ class uSvgGraph{
     let defaultStyle = {
       stroke:"#000", "stroke-width":"2"
     }
-    style = {...defaultStyle, ...style}
+    style = {...defaultStyle, ...style};
 
     let ymin = f.y(this.xmin);
     let xmin = this.xmin;
@@ -211,6 +211,28 @@ class uSvgGraph{
     let newLine = this.addLine(p1, p2, {style: style});
     if (this.lineList === undefined){this.lineList = []};
     this.lineList.push(newLine);
+
+  }
+
+  labelLinearFunction(f = new uLine(), x = 0, offset= new uPoint(0.25,0.25), {style = {}} = {}){
+    let defaultStyle = {
+      "text-anchor": "start",
+      "font-size": "10px",
+      fill: "red"
+    }
+    style = {...defaultStyle, ...style};
+
+    let loc = new uPoint(x, f.y(x));
+    loc = loc.add(offset);
+
+    let txt = f.eqnAsText();
+
+    let t = this.addText(txt, loc, {style: style})
+
+    if (this.pointLabelsList === undefined){this.pointLabelsList = []};
+    this.pointLabelsList.push(t);
+    return t;
+
   }
 
   labelPoint(p = new uPoint(), offset= new uPoint(0.25,0.25), {style = {}} = {}){
